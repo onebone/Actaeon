11849b8c568ba8a9c757e11addc4bc0f19ea6501
onebone <jyc00410@gmail.com>
2016-11-09 23:35:46 +0900

    Fixed entity stuck in ground after server restart

--- src/main/java/me/onebone/actaeon/entity/MovingEntity.java
+++ src/main/java/me/onebone/actaeon/entity/MovingEntity.java
@@ -7,7 +7,6 @@ import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.math.AxisAlignedBB;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.tag.CompoundTag;
-import cn.nukkit.timings.Timings;
 import me.onebone.actaeon.route.RouteFinter;
 import me.onebone.actaeon.route.SimpleRouteFinder;
 
@@ -37,21 +36,6 @@ abstract public class MovingEntity extends EntityCreature{
 	public boolean onUpdate(int currentTick){
 		if(this.closed){
 			return false;
-		}
-
-		AxisAlignedBB[] list = this.level.getCollisionCubes(this, this.level.getTickRate() > 1 ? this.boundingBox.getOffsetBoundingBox(0, -1, 0) : this.boundingBox.addCoord(0, -1, 0), false);
-
-		double maxY = 0;
-		for(AxisAlignedBB bb : list){
-			if(bb.maxY > maxY){
-				maxY = bb.maxY;
-			}
-		}
-
-		this.onGround = (maxY == this.boundingBox.minY);
-
-		if(!this.onGround){
-			this.motionY -= this.getGravity();
 		}
 
 		if(this.isKnockback){                   // knockback 이 true 인 경우는 맞은 직후
@@ -103,6 +87,12 @@ abstract public class MovingEntity extends EntityCreature{
 
 		this.move(this.motionX, this.motionY, this.motionZ);
 
+		this.addMotion(this.motionX, this.motionY, this.motionZ);
+
+		if(!this.onGround){
+			this.motionY -= this.getGravity();
+		}
+
 		if(this.onGround){
 			if(!this.route.isSearching()){
 				Entity[] entities = this.level.getNearbyEntities(new AxisAlignedBB(this.x, this.y, this.z, this.x, this.y, this.z).expand(7, 7, 7));
@@ -146,6 +136,17 @@ abstract public class MovingEntity extends EntityCreature{
 		this.isCollidedHorizontally = (movX != dx || movZ != dz);
 		this.isCollided = (this.isCollidedHorizontally || this.isCollidedVertically);
 
+		AxisAlignedBB[] list = this.level.getCollisionCubes(this, this.level.getTickRate() > 1 ? this.boundingBox.getOffsetBoundingBox(0, -1, 0) : this.boundingBox.addCoord(0, -1, 0), false);
+
+		double maxY = 0;
+		for(AxisAlignedBB bb : list){
+			if(bb.maxY > maxY){
+				maxY = bb.maxY;
+			}
+		}
+
+		this.onGround = (maxY == this.boundingBox.minY);
+
 		// this.onGround = (movY != dy && movY < 0);
 		// onGround 는 onUpdate 에서 확인
 	}
@@ -165,120 +166,7 @@ abstract public class MovingEntity extends EntityCreature{
 	}
 
 	@Override
-	public boolean move(double dx, double dy, double dz) {
-		if (dx == 0 && dz == 0 && dy == 0) {
-			return true;
-		}
-
-		if (this.keepMovement) {
-			this.boundingBox.offset(dx, dy, dz);
-			this.setPosition(this.temporalVector.setComponents((this.boundingBox.minX + this.boundingBox.maxX) / 2, this.boundingBox.minY, (this.boundingBox.minZ + this.boundingBox.maxZ) / 2));
-			this.onGround = this.isPlayer;
-			return true;
-		} else {
-
-			Timings.entityMoveTimer.startTiming();
-
-			this.ySize *= 0.4;
-
-			double movX = dx;
-			double movY = dy;
-			double movZ = dz;
-
-			AxisAlignedBB axisalignedbb = this.boundingBox.clone();
-
-			AxisAlignedBB[] list = this.level.getCollisionCubes(this, this.level.getTickRate() > 1 ? this.boundingBox.getOffsetBoundingBox(dx, dy, dz) : this.boundingBox.addCoord(dx, dy, dz), false);
-
-			for (AxisAlignedBB bb : list) {
-				 dy = bb.calculateYOffset(this.boundingBox, dy);
-			}
-
-			this.boundingBox.offset(0, dy, 0);
-
-			boolean fallingFlag = (this.onGround || (dy != movY && movY < 0));
-
-			for (AxisAlignedBB bb : list) {
-				dx = bb.calculateXOffset(this.boundingBox, dx);
-			}
-
-			this.boundingBox.offset(dx, 0, 0);
-
-			for (AxisAlignedBB bb : list) {
-				dz = bb.calculateZOffset(this.boundingBox, dz);
-			}
-
-			this.boundingBox.offset(0, 0, dz);
-
-			if (this.getStepHeight() > 0 && fallingFlag && this.ySize < 0.05 && (movX != dx || movZ != dz)) {
-				double cx = dx;
-				double cy = dy;
-				double cz = dz;
-				dx = movX;
-				dy = this.getStepHeight();
-				dz = movZ;
-
-				AxisAlignedBB axisalignedbb1 = this.boundingBox.clone();
-
-				this.boundingBox.setBB(axisalignedbb);
-
-				list = this.level.getCollisionCubes(this, this.boundingBox.addCoord(dx, dy, dz), false);
-
-				for (AxisAlignedBB bb : list) {
-					dy = bb.calculateYOffset(this.boundingBox, dy);
-				}
-
-				this.boundingBox.offset(0, dy, 0);
-
-				for (AxisAlignedBB bb : list) {
-					dx = bb.calculateXOffset(this.boundingBox, dx);
-				}
-
-				this.boundingBox.offset(dx, 0, 0);
-
-				for (AxisAlignedBB bb : list) {
-					dz = bb.calculateZOffset(this.boundingBox, dz);
-				}
-
-				this.boundingBox.offset(0, 0, dz);
-
-				this.boundingBox.offset(0, 0, dz);
-
-				if ((cx * cx + cz * cz) >= (dx * dx + dz * dz)) {
-					dx = cx;
-					dy = cy;
-					dz = cz;
-					this.boundingBox.setBB(axisalignedbb1);
-				} else {
-					this.ySize += 0.5;
-				}
-
-			}
-
-			this.x = (this.boundingBox.minX + this.boundingBox.maxX) / 2;
-			this.y = this.boundingBox.minY - this.ySize - this.getHeight();
-			this.z = (this.boundingBox.minZ + this.boundingBox.maxZ) / 2;
-
-			this.checkChunks();
-
-			this.checkGroundState(movX, movY, movZ, dx, dy, dz);
-			this.updateFallState(this.onGround);
-
-			if (movX != dx) {
-				this.motionX = 0;
-			}
-
-			if (movY != dy) {
-				this.motionY = 0;
-			}
-
-			if (movZ != dz) {
-				this.motionZ = 0;
-			}
-
-
-			//TODO: vehicle collision events (first we need to spawn them!)
-			Timings.entityMoveTimer.stopTiming();
-			return true;
-		}
+	public void addMovement(double x, double y, double z, double yaw, double pitch, double headYaw) {
+		this.level.addEntityMovement(this.chunk.getX(), this.chunk.getZ(), this.id, x, y - this.getHeight(), z, yaw, pitch, headYaw);
 	}
 }
